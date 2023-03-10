2c3b338c65d9ad03a0d5ed0cf552bd761ebe2a5f
hanvidlee <hanvidlee@gmail.com>
2023-03-10 00:48:02 -0500

    counting letters in string

---
+++ countLetters.js
@@ -0,0 +1,13 @@
+const countLetters = function(string) {
+  let countOfLetter = {}
+  for(const letter of string) {
+    if(countOfLetter[letter]) {
+      countOfLetter[letter] += 1
+    } else {
+      countOfLetter[letter] = 1
+    }
+  }
+  return countOfLetter
+}
+
+console.log(countLetters("LHL"))
