dac046952d05623d410042b4d23d38a8fb85bd35
hanvidlee <hanvidlee@gmail.com>
2023-03-09 20:33:07 -0500

    finding key using callback

---
+++ findKey.js
@@ -0,0 +1,57 @@
+const findKey = function (object, callback) {
+  const result = []
+  for( const key in object) {
+    if(callback(object[key])){
+      result.push(key)
+      break
+    }
+  }
+  return result
+}
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2))
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const assertArraysEqual = function(actual,expected){
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(first,second) {
+  if (first.length !== second.length) {
+    return false
+  }
+  
+  for (let i = 0; i < first.length; i++) {
+    if (first[i] !== second[i]) {
+      return false
+    }
+  }
+  return true
+}
+
+assertArraysEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), ["noma"])
