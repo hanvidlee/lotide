d2db5e17cf9e65bc8f390b3f44ff0a41f7deee21
hanvidlee <hanvidlee@gmail.com>
2023-03-10 01:26:04 -0500

    letter position function

---
+++ letterPositions.js
@@ -0,0 +1,14 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  // logic to update results here
+  for (let i = 0; i < sentence.length; i++){
+    letter = sentence[i]
+    if (results[letter]) {
+      results[letter].push(i)
+    } else {
+      results[letter] = [i]
+    }
+  }
+  return results;
+}
+console.log(letterPositions("hello"))
