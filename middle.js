2c7bab59233b09c95374b0deef9b3d93e9999eba
hanvidlee <hanvidlee@gmail.com>
2023-03-13 18:38:31 -0400

    refactored

--- middle.js
+++ middle.js
@@ -31,3 +31,5 @@ console.log(middle([1])) // => []
 console.log(middle([1, 2])) // => []
 console.log(middle([1, 2, 3])) // => [2]
 console.log(middle([1, 2, 3, 4, 5])) // => [3]
+
+module.exports = middle
